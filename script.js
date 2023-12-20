9cd076f03b5066aa3fddb9726f7a3efc08dbcb48
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-20 21:03:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const makeChange = (c) => {
   // your name here
+	function makeChange(amount) {
+  // Initialize variables to store the count of each coin
+  let quarters = 0;
+  let dimes = 0;
+  let nickels = 0;
+  let pennies = 0;
+
+  // Calculate the number of quarters
+  quarters = Math.floor(amount / 25);
+  // Calculate the remaining amount after using quarters
+  amount %= 25;
+
+  // Calculate the number of dimes
+  dimes = Math.floor(amount / 10);
+  // Calculate the remaining amount after using dimes
+  amount %= 10;
+
+  // Calculate the number of nickels
+  nickels = Math.floor(amount / 5);
+  // Calculate the remaining amount after using nickels
+  amount %= 5;
+
+  // The remaining amount is the number of pennies
+  pennies = amount;
+
+  // Return the result as an object
+  return { q: quarters, d: dimes, n: nickels, p: pennies };
+}
 };
 
 // Do not the change the code below
